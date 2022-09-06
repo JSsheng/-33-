00f6e42de9fbe89f830b8ac8e98dc875b216501b
haiou <3529591183@qq.com>
2022-09-06 19:18:09 +0800

    验证规则完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
-
-const routes = []
+// SPA
+// 路由懒加载
+//  - 使用到了路由页面，再去请求
+const routes = [
+  {
+    path: '/login',
+    component: () => import('@/views/login')
+  }
+]
 
 const router = new VueRouter({
   routes
---
+++ src/views/login/index.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <!-- 头部导航 -->
+    <van-nav-bar title="登录" class="nav-bar" />
+    <!-- 表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="mobile"
+        name="mobile"
+        placeholder="请输入电话号码"
+        :rules="[
+          { required: true, message: '请填写用户名' },
+          {
+            pattern:
+              /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
+            message: '手机格式错误'
+          }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-shouji"></span>
+        </template>
+      </van-field>
+      <van-field
+        v-model="code"
+        name="code"
+        placeholder="请输入验证码"
+        :rules="[
+          { required: true, message: '请填写密码' },
+          { pattern: /[0-9]{6}/, message: '验证码格式错误' }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-yanzhengma"></span>
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button round block type="info" native-type="submit"
+          >提交</van-button
+        >
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      mobile: '',
+      code: ''
+    }
+  },
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.nav-bar {
+  background-color: #3296fa;
+
+  ::v-deep .van-nav-bar__title {
+    color: #fff;
+  }
+}
+</style>
